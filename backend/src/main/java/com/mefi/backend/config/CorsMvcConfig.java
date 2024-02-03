c2a46b920ae07902e8faf4f50933d2e214036a47
권현준 <bonheur9813@naver.com>
2024-02-03 14:18:04 +0900

    change2

--- backend/src/main/java/com/mefi/backend/config/CorsMvcConfig.java
+++ backend/src/main/java/com/mefi/backend/config/CorsMvcConfig.java
@@ -1,6 +1,7 @@
 package com.mefi.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
